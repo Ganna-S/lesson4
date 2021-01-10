a0396e87560afd0bb4c56dbb191416766b6fd3b4
Ganna-S <gannashutenko@gmail.com>
2021-01-10 15:55:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,131 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// console.log(nameT);
+// var nameT = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// alert(5);
+// [].push('a');
+
+/* let number = 4.6;
+
+console.log(-4/0); 
+console.log('srting' * 9);
+
+const persone = "Alex";
+
+const bool = true;
+
+console.log(something);
+
+let und;
+console.log(und); */
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarred: false
+// };
+
+// /* console.log(obj.name); */
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+/* alert('Hello'); */
+
+// const result = confirm("Are you heare?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+//Интерполяция
+
+// const category = 'toys';
+// console.log(`https://somuurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + " - object");
+
+// console.log(4 + " - object");
+
+// console.log(4 + +"5");
+
+// // инкремент и дикременит
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(5%2);
+
+//  console.log(2*4 === '8');
+
+// && и
+// || или
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || isClose);
+
+// // ! оператор отрицания
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 != 8);
